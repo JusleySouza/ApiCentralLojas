6a49fc1f188c32b7c328b7b60bae5fa19f0c8c22
Jusley Souza <jusleysouza31@gmail.com>
2022-01-16 20:38:13 -0300

    Starting the project

---
+++ src/test/java/com/central/lojas/CentralLojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.central.lojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralLojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
