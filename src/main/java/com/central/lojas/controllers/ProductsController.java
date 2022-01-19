27ab2e09959a2fad1de994b5ac48b60354f4fcf9
Jusley Souza <jusleysouza31@gmail.com>
2022-01-19 19:59:59 -0300

    Description of services for swagger and base route change

--- src/main/java/com/central/lojas/controllers/ProductsController.java
+++ src/main/java/com/central/lojas/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.central.lojas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,34 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.central.lojas.models.Product;
 import com.central.lojas.services.ProductServices;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
 @RestController
-@RequestMapping(value="/central-lojas/produtos")
+@RequestMapping(value="/produtos")
+@Api(value="API CENTRAL LOJAS produtos")
+@CrossOrigin(origins="*")
 public class ProductsController {
 	
 	@Autowired
 	ProductServices productServices;
 
 	@GetMapping
+	@ApiOperation(value="Listar produtos")
 	public List<Product> listProducts(){
 		return productServices.allProducts();
 	}
 		
 	@GetMapping("/{id}")
+	@ApiOperation(value="Filtrar produto")
 	public Product findById(@PathVariable(value="id")long id){
 		return productServices.findProduct(id);	
 	}
 	
 	@PostMapping
+	@ApiOperation(value="Salvar produto")
 	public  Product saveProduct(@RequestBody Product product) {
 		return productServices.saveProduct(product);
 	}
 	
 	@DeleteMapping("/{id}")
+	@ApiOperation(value="Deletar produto")
 	public void deleteProduct(@PathVariable(value="id")long id) {
 		productServices.deleteProduct(id);
 	}
 	
 	@PutMapping
+	@ApiOperation(value="Atualizar produtos")
 	public Product updateProduct(@RequestBody Product product) {
 		return productServices.saveProduct(product);
 	}
