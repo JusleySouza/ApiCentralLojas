eb170d27635dba48405d2fbd3197fdf68428b5e4
Jusley Souza <jusleysouza31@gmail.com>
2022-01-18 21:59:48 -0300

    Repository creation

---
+++ src/main/java/com/central/lojas/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.central.lojas.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.central.lojas.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+}
