b3e1ff07c2acdeaee447c1d90568cd520063b499
Jusley Souza <jusleysouza31@gmail.com>
2022-01-18 23:14:37 -0300

    Creating the route to delete a product

--- src/main/java/com/central/lojas/controllers/ProductsController.java
+++ src/main/java/com/central/lojas/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.central.lojas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,10 @@ public class ProductsController {
 	@PostMapping
 	public  Product saveProduct(@RequestBody Product product) {
 		return productServices.saveProduct(product);
+	}
+	
+	@DeleteMapping
+	public void deleteProduct(@RequestBody Product product) {
+		productServices.deleteProduct(product);
 	}
 }
